5e677db55d6af8e804434ff53253f7e4bcdf4841
Csató Áron <csatoaron@gmail.com>
2023-01-27 22:07:37 +0100

    style.css fájl módosítva, html fájl átszerkesztve.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const arr = [
 
     {
         name: "Mars",
-        size: "Felfedezési ideje: 1610",
-        weight: "Tömege: 6,39E23 kg (a földi 0,107-szerese)",
-        time: "687 nap",
-        description: "A Mars a Naptól számított negyedik bolygó a Naprendszerben. Szabad szemmel is könnyedén látható az éjszakai égbolton. A római hadistenről nevezték el, de gyakran hívják „vörös bolygónak” is színe miatt, amit a Mars felszínét meghatározó vas-oxid okoz. A Mars a harmadik legnagyobb kőzetbolygó, számos rendkívüli felszíni képződménnyel.Mivel a Mars feleakkora átmérőjű, mint a Föld, ezért felszíne negyede, a tömege kb. nyolcada a Földének. A talajminták vizsgálata alapján a Mars talaja nagy valószínűséggel alkalmas az életre, növények termesztésére: az Antarktika szárazabb völgyeiben található talajhoz hasonló tulajdonságokat mutat. Lúgos kémhatású (az adott elemzés 8–9 közötti pH-értékeket mutatott ki), kimutatható magnézium, nátrium és kálium, az élethez szükséges tápanyagokat és vegyületeket is találtak. A marsi élet csak mai értelmében jelent idegen életet, mert nagyon sok terv született a Mars emberek által való kolonizálására. Azért a Marsot tartják a legalkalmasabbnak kolonizálás szempontjából, mivel itt található legközelebb víz, valamint ez a legközelebbi szilárd bolygó. Ettől függetlenül a Mars felszínén nem tud megélni növény vagy állat a jelenlegi környezeti feltételek miatt.[30] A bolygónak a gravitációja 38% a Földhöz képest, mely eltérés izomvesztéssel és csontlerakódással jár.[31] A kolonizálóknak ezen kívül megoldást kell találniuk a nagy hőmérséklet-különbségre, ugyanis a Marson sokkal nagyobb a hőingadozás, ami átlagosan −87 és −5 °C között van. Több vállalat foglalkozik a projekttel, ezek közül a SpaceX vállalat akar először a Marsra jutni. Elon Musk, a cég vezérigazgatója 2016 júniusában jelentette be, hogy 2025-ben embereket küldenek a bolygóra. A terv szerint 100 fős űrhajókban utaznának a jövőbeli lakosok, akiknek 200 ezer dollárt kellene fizetniük a költözésért. Bár kezdetben ennél sokkal drágább lesz az utazás, Musk úgy gondolja, nem lehet benépesíteni egy bolygót, ha csak milliárdosok költözhetnek oda. Ezért mindenképpen lejjebb kell vinni az árat, nagyjából egy amerikai lakás árára.",
+        discovered: "Felfedezés dátuma: 1610",
+        size: "Felszín területe: 144 800 000 km²",
+        time: "Keringési idő: 687 nap",
+        description: "A Mars a Naptól számított negyedik bolygó a Naprendszerben. Szabad szemmel is könnyedén látható az éjszakai égbolton. A római hadistenről nevezték el, de gyakran hívják „vörös bolygónak” is színe miatt, amit a Mars felszínét meghatározó vas-oxid okoz. Több vállalat foglalkozik a projekttel, ezek közül a SpaceX vállalat akar először a Marsra jutni. Elon Musk, a cég vezérigazgatója 2016 júniusában jelentette be, hogy 2025-ben embereket küldenek a bolygóra. A terv szerint 100 fős űrhajókban utaznának a jövőbeli lakosok, akiknek 200 ezer dollárt kellene fizetniük a költözésért. Bár kezdetben ennél sokkal drágább lesz az utazás, Musk úgy gondolja, nem lehet benépesíteni egy bolygót, ha csak milliárdosok költözhetnek oda. Ezért mindenképpen lejjebb kell vinni az árat, nagyjából egy amerikai lakás árára.",
         img:"https://www.mkewithkids.com/wp-content/uploads/2022/11/planet-mars.jpg"
 
 
@@ -14,9 +14,9 @@ const arr = [
 
     {
         name: "Phobos",
-        size: "Felfedezési ideje: 1877. augusztus 17.",
-        weight: " Tömege: 1.0659 * 10 `<sup>16</sup> kg",
-        time: "Keringési idő: 7 óra 39 perc",
+        discovered: "Felfedezés dátuma:1877.08.17.",
+        size: "Felszíni területe:26,8 × 22,4 × 18,4km",
+        time: "Keringési idő:7 óra 39 perc",
         description: "A Phobos a Mars nagyobbik holdja. A Mars felszíne és a Phobos közötti távolság mindössze 5760 km. Összehasonlításképpen: bolygónk és a Hold között 378 000 km ez a távolság. Mars körüli ellipszis alakú pályájának excentricitása 0,0210, fél nagytengelye 9382 km. A pályasíkja csaknem egybeesik a bolygó keringési síkjával. A Phobos gyorsabban kering, mint ahogy a Mars egyszer megfordul a tengelye körül. A Marsot 7 h 39 m alatt kerüli meg, vagyis naponta háromszor, eközben kétszer kel fel a nyugati és kétszer nyugszik le a keleti égbolton. A legújabb kutatási eredmények szerint nem becsapódás hozta létre a Phobos felszínén található barázdákat, mert azok kora jelentős ingadozást mutat. A legfiatalabb barázdák ugyanis maximum néhány millió évesek. Azokat a Mars és a Phobos között zajló árapály erők hozták létre, ami a hold szétszakadásához fog vezetni. Ugyanis a Phobos belső szerkezete sokkal szivacsosabb, mint korábban gondolták. Valószínűleg viszonylag szorosan egymáshoz illeszkedő darabokból állhat, így szétszakadása könnyebben megtörténik.",
         img: "https://astronomynow.com/wp-content/uploads/2016/10/Phobos_Stickney-crater_667x500.jpg"
 
@@ -26,10 +26,10 @@ const arr = [
 
     {
         name: "Deimos",
-        size: "Felfedezési ideje: 1877. augusztus 12.",
-        weight: " Tömege: 2 * 10**15 kg  es `<sup>15</sup>15`",
-        time: " Keringési idő: 30 óra 21 perc",
-        description: "A Deimos a Mars kisebbik holdja. Csakúgy, mint a nagyobb Mars-hold, a Phobos, a Deimos is kisméretűnek számít a Naprendszer holdjai között. A Mars mindkét holdját Asaph Hall amerikai csillagász, az Egyesült Államok Tengerészeti Obszervatóriumának (United States Naval Observatory) kutatója fedezte fel 1877. augusztus 12-én. A Phobos ellentétben keleten kel és nyugaton nyugszik a Mars égboltján. A Deimos távolabb mozog bolygójától, keringési ideje hosszabb, mint a Mars forgási ideje, ezért a bolygóról nézve lassabban mozog, mint a csillagok. 5,4 naponta kel fel és ezután 2,7 napig látszik az égen. Felszíne tele van kisebb kráterekkel és világosabb foltokkal. A világos foltok meteorit becsapódáskor a felszínre hullt finomabb anyagok lehetnek. A másik holddal ellentétben kőtömbök is vannak rajta. Két jelentősebb krátere a felvilágosodás két neves írójáról, Voltaire-ről és Swiftről kapta a nevét. Maga a felszín sötét színű, néhány tíz m vastag regolitból áll, ami meteoritok becsapódásakor létrejött kő és portörmelék. A hold magas széntartalmú kőzetekből áll, sűrűségük hozzávetőleg 2 g/cm³. Így a szenes kondritok csoportjába tartozik, ami a meteoritok egy fajtája. Hasonló szerkezetű égitestek a Jupiter kisbolygóövének külső részében találhatók. Ebből kifolyólag valaha valamelyik másik bolygóból kiszakadt meteor lehetett, de a Mars gravitációs mezejének csapdájába került.",
+        discovered: "Felfedezés dátuma:1877.08.12.",
+        size: "Felszíni területe: 27,0 × 21,6 × 18,8 km",
+        time: "Keringési idő:30 óra 21 perc",
+        description: "A Deimos a Mars kisebbik holdja. Csakúgy, mint a nagyobb Mars-hold, a Phobos, a Deimos is kisméretűnek számít a Naprendszer holdjai között. A Mars mindkét holdját Asaph Hall amerikai csillagász, az Egyesült Államok Tengerészeti Obszervatóriumának (United States Naval Observatory) kutatója fedezte fel 1877. augusztus 12-én. A Phobos ellentétben keleten kel és nyugaton nyugszik a Mars égboltján. A Deimos távolabb mozog bolygójától, keringési ideje hosszabb, mint a Mars forgási ideje, ezért a bolygóról nézve lassabban mozog, mint a csillagok. 5,4 naponta kel fel és ezután 2,7 napig látszik az égen. Felszíne tele van kisebb kráterekkel és világosabb foltokkal. A világos foltok meteorit becsapódáskor a felszínre hullt finomabb anyagok lehetnek. A másik holddal ellentétben kőtömbök is vannak rajta. Két jelentősebb krátere a felvilágosodás két neves írójáról, Voltaire-ről és Swiftről kapta a nevét.",
         img: "https://upload.wikimedia.org/wikipedia/commons/8/86/NASA-Deimos-MarsMoon-20090221.jpg"
 
 
@@ -39,7 +39,7 @@ const arr = [
 
 const nev = document.getElementById("name");
 const img = document.getElementById("img");
-const weight = document.getElementById("weight");
+const size = document.getElementById("size");
 const time = document.getElementById("time");
 const description = document.getElementById("description");
 const discovered = document.getElementById("discovered");
@@ -52,7 +52,7 @@ window.addEventListener("load", ()=>{
 
     const item = arr[current];
     nev.innerText = item.name;
-    weight.innerText = item.weight;
+    size.innerText = item.size;
     time.innerText = item.time;
     description.innerText = item.description;
     discovered.innerText = item.discovered;
@@ -64,7 +64,7 @@ function showPlanet(planet){
 
     const item = arr[planet];
     nev.textContent = item.name;
-    weight.textContent = item.weight;
+    size.textContent = item.size;
     time.textContent = item.time;
     description.textContent = item.description;
     discovered.textContent = item.discovered;
